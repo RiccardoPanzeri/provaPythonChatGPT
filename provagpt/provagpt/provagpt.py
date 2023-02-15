7cd7e0a80adfe347f5e00c0b8929964d00b61d50
Riccardo Panzeri <riccardo.panzeri@outlook.it>
2023-02-15 03:06:54 +0100

    aggiunto file .py principale

---
+++ provagpt/provagpt/provagpt.py
@@ -0,0 +1,33 @@
+import openai
+#affinchè il programma funzioni, è necessario inserire qui la chiave API OpenAi personale.
+openai.api_key = "Inserisci la tua chiave API OpenAI"
+#creiamo le variabili necessarie, che contengono: 
+#il modello dell'engine di completamento automatico di OpenAI
+modelloEngine = "text-davinci-003"
+restart = True;
+while(restart == True):
+#e l'input che verrà inserito dall'utente per interagire con l'AI
+    domanda = input("Chiedi qualcosa all'AI: ")
+
+#con questa porzione di codice, andiamo a generare una risposta all'input, aggiustando i parametri di comportamento del bot: 
+    completion = openai.Completion.create(
+        engine = modelloEngine,
+        prompt = domanda,
+        max_tokens = 1024,
+        n= 1,
+        stop = None,
+        temperature = 0.5,#la temperatura influisce sulla coerenza della risposta: più è alta, più varia ma meno accurata sarà la risposta all'input.
+    )
+
+
+    risposta = completion.choices[0].text #inseriamo la risposta generata in una variabile;
+
+    print(risposta)
+
+    
+
+
+
+
+
+
